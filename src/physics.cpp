a39d312a94968083e23789c56932db621888a63b
aruiz <aruiz@PB1-22>
2017-02-22 10:10:05 +0100

    Add project files.

---
+++ src/physics.cpp
@@ -0,0 +1,35 @@
+#include <imgui\imgui.h>
+#include <imgui\imgui_impl_glfw_gl3.h>
+
+namespace LilSpheres {
+	extern const int maxParticles;
+	extern void setupParticles(int numTotalParticles, float radius = 0.05f);
+	extern void cleanupParticles();
+	extern void updateParticles(int startIdx, int count, float* array_data);
+	extern void drawParticles(int startIdx, int count);
+}
+
+bool show_test_window = true;
+void GUI() {
+	{	//FrameRate
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+
+		//TODO
+	}
+
+	// ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+	if(show_test_window) {
+		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+		ImGui::ShowTestWindow(&show_test_window);
+	}
+}
+
+void PhysicsInit() {
+	//TODO
+}
+void PhysicsUpdate(float dt) {
+	//TODO
+}
+void PhysicsCleanup() {
+	//TODO
+}
